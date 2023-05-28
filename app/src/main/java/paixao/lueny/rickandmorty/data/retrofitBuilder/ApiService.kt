141335f240d0b563789852ff4ed2f6e39ab86caf
Lueny Dantas <luenypaixao@gmail.com>
2023-05-27 23:57:18 -0300

    : Fix: Changes in ApiService

--- app/src/main/java/paixao/lueny/rickandmorty/data/retrofitBuilder/ApiService.kt
+++ app/src/main/java/paixao/lueny/rickandmorty/data/retrofitBuilder/ApiService.kt
@@ -1,5 +1,12 @@
 package paixao.lueny.rickandmorty.data.retrofitBuilder
 
-interface ApiService{
+import paixao.lueny.rickandmorty.data.model.ApiCharactersResponse
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface ApiService {
+
+    @GET("api/character")
+    suspend fun getCharacters(@QueryMap params: Map<String, String> = emptyMap()): ApiCharactersResponse
 
 }
