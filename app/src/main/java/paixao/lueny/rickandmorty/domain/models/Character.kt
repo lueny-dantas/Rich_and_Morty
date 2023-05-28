82e4c470a98c768c7deda51b33618b2dcb1d48a3
Lueny Dantas <luenypaixao@gmail.com>
2023-05-27 23:42:15 -0300

    Feat: Implementation of domain

---
+++ app/src/main/java/paixao/lueny/rickandmorty/domain/models/Character.kt
@@ -0,0 +1,32 @@
+package paixao.lueny.rickandmorty.domain.models
+
+data class Character(
+    val name: String,
+    val status: Status,
+    val species: String,
+    val gender: Gender,
+    val origin: Origin,
+    val location: Location,
+    val image: String,
+) {
+    enum class Status(val statusPresentation: String) {
+        Alive("Vivo"),
+        Dead("Morto"),
+        Unknown("Desconhecido")
+    }
+
+    enum class Gender(val genderApresentation: String) {
+        Female("Fêmea"),
+        Male("Macho"),
+        Genderless("Sem Gênero"),
+        Unknown("Desconhecido")
+    }
+
+    data class Origin(
+        val name: String
+    )
+
+    data class Location(
+        val name: String
+    )
+}
