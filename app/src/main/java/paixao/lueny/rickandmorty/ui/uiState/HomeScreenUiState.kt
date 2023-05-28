14ac9d68bc9c9a0aeaf632bbe62fc277aa9ee84b
Lueny Dantas <luenypaixao@gmail.com>
2023-05-27 23:44:27 -0300

    Feat: Implementation of HomeScreenUiState

---
+++ app/src/main/java/paixao/lueny/rickandmorty/ui/uiState/HomeScreenUiState.kt
@@ -0,0 +1,22 @@
+package paixao.lueny.rickandmorty.ui.uiState
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+
+
+/*
+sealed interface HomeScreenUiState {
+    data class Success(val characters: String) : HomeScreenUiState
+    data class Loading : HomeScreenUiState
+    data class Error : HomeScreenUiState
+}
+
+val _uiState: HomeScreenUiState by mutableStateOf(HomeScreenUiState.Loading)
+    private set
+
+private fun getCharacters(){
+
+}
+*/
+
+
