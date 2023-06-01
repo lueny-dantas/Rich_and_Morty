c94211ec7ec6d7ce94b04db5bc6289390ab0b20e
Lueny Dantas <luenypaixao@gmail.com>
2023-05-31 23:26:16 -0300

    Feat: Implementation of FilterBottonSheet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui:1.0.0")
+    implementation("androidx.compose.material:material:1.0.0")
+
 
     //tests
     testImplementation("junit:junit:4.13.2")
--- app/src/main/java/paixao/lueny/rickandmorty/ui/screens/charactersScreen/CharactersScreen.kt
+++ app/src/main/java/paixao/lueny/rickandmorty/ui/screens/charactersScreen/CharactersScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -17,20 +16,31 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.List
+import androidx.compose.material.rememberModalBottomSheetState
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.semantics.Role.Companion.Checkbox
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,11 +49,11 @@ import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
 import paixao.lueny.rickandmorty.R
-import paixao.lueny.rickandmorty.data.model.CharactersResponse
 import paixao.lueny.rickandmorty.domain.models.Character
 import paixao.lueny.rickandmorty.ui.theme.RickandMortyTheme
 import paixao.lueny.rickandmorty.ui.theme.caveatFont
 import paixao.lueny.rickandmorty.ui.theme.teal_700
+import androidx.compose.foundation.layout.Column as Column
 
 @Composable
 fun CharactersScreen(
@@ -101,20 +111,19 @@ private fun Toolbar(context: Context, onFilterClick: () -> Unit) {
 
 
 @Composable
-private fun CharacterList(characters: LazyPagingItems<Character>){
+private fun CharacterList(characters: LazyPagingItems<Character>) {
 
     LazyColumn(
         contentPadding = PaddingValues(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp),
-        ){
-        items(characters){ character ->
+    ) {
+        items(characters) { character ->
             character?.let {
                 CharacterItem(character = character)
             }
         }
     }
 }
-
 
 
 @Composable
@@ -151,6 +160,80 @@ private fun FilterButton(onClick: () -> Unit) {
         }
     }
 }
+
+
+@Composable
+fun BottomSheet() {
+    val context = LocalContext.current
+    var isBottomSheetVisible by remember { mutableStateOf(false) }
+
+    Column {
+        Button(
+            onClick = { isBottomSheetVisible = true },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Text(context.getString(R.string.open_filter))
+        }
+
+        if (isBottomSheetVisible) {
+            FilterBottomSheet(
+                onClose = { isBottomSheetVisible = false },
+                onApplyFilter = { /* Lógica para aplicar o filtro */ })
+        }
+    }
+
+}
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun FilterBottomSheet(onClose: () -> Unit, onApplyFilter: () -> Unit) {
+    val state = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
+
+    ModalBottomSheetLayout(
+        sheetState = state ,
+        sheetContent = {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = "Filter Options",
+                    style = TextStyle(fontSize = 20.sp),
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+
+                // Aqui você pode adicionar opções de filtro, como caixas de seleção, sliders, etc.
+                // Exemplo:
+                Checkbox(
+                    checked = true,
+                    onCheckedChange = { /* Lógica para tratar a seleção do filtro */ },
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+            }
+        },
+        sheetShape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
+        scrimColor = Color.Black.copy(alpha = 0.5f)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+        ) {
+            Button(
+                onClick = {
+                    onApplyFilter()
+                    onClose()
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "Apply Filter")
+            }
+        }
+    }
+}
+
 
 @Preview
 @Composable
