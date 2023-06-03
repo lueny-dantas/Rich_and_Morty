81721ab77c5bc1356091577091e9d7de51544082
Lueny Dantas <luenypaixao@gmail.com>
2023-06-03 02:02:03 -0300

    fix: Changes of CharactersDetailsUiState.kt

--- app/src/main/java/paixao/lueny/rickandmorty/ui/screens/characterDetailsScreen/CharacterDetailsUiState.kt
+++ app/src/main/java/paixao/lueny/rickandmorty/ui/screens/characterDetailsScreen/CharacterDetailsUiState.kt
@@ -3,7 +3,7 @@ package paixao.lueny.rickandmorty.ui.screens.characterDetailsScreen
 import paixao.lueny.rickandmorty.domain.models.Character
 
 data class CharacterDetailsUiState(
-    val character: List<Character> = emptyList()
+    val character: List<Character> = emptyList(),
 )
 
 
