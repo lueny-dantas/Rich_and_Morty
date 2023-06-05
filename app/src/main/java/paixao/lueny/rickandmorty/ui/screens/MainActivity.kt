5c585bc028bd7861e1bab518c665b0388afebcf7
Lueny Dantas <luenypaixao@gmail.com>
2023-06-05 04:04:55 -0300

    chore: move main activity

--- app/src/main/java/paixao/lueny/rickandmorty/ui/screens/MainActivity.kt
+++ app/src/main/java/paixao/lueny/rickandmorty/ui/screens/MainActivity.kt
@@ -1,28 +1,17 @@
-package paixao.lueny.rickandmorty
+package paixao.lueny.rickandmorty.ui.screens
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import kotlinx.coroutines.launch
-import paixao.lueny.rickandmorty.domain.models.Character
 import paixao.lueny.rickandmorty.ui.navigation.AppNavHost
-import paixao.lueny.rickandmorty.ui.navigation.navigateToCharacterDetails
 import paixao.lueny.rickandmorty.ui.theme.RickandMortyTheme
 
 class MainActivity : ComponentActivity() {
